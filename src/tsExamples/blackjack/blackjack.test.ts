25a0194fd854300d8c4e74ff3bab0640618d6420
Chris Sellek <iamsellek@gmail.com>
2021-09-08 14:28:50 -0400

    Add mocking example, which I swear was here at some point before the actual workshop. This is fine.

--- src/tsExamples/blackjack/blackjack.test.ts
+++ src/tsExamples/blackjack/blackjack.test.ts
@@ -1,7 +1,17 @@
-import { getSumOfCardValues } from './blackjack';
+import * as blackjackServices from './blackjack';
+import { getHandResult, getSumOfCardValues } from './blackjack';
 
 describe('tests', () => {
   test('getSumOfCardValues', () => {
     expect(getSumOfCardValues([])).toBe(0);
+  });
+
+  test('getHandResult', () => {
+    const spy = jest.spyOn(blackjackServices, 'getSumOfCardValues');
+
+    spy.mockReturnValueOnce(21); // player score
+    spy.mockReturnValueOnce(17); // dealer score
+
+    expect(getHandResult([], [])).toBe('win');
   });
 });
